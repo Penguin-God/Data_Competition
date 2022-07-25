3106172e6a55941b7f52ad7dcb6a2d192870e763
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-07-25 23:07:56 +0900

    글자 크기 변경

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ class Region:
         self.regionName = path[0] + path[1]
         self.data = pd.read_csv(path, encoding="utf-8")
     
-    def Pirvate_GetCategoryDict(self, data, mainName, subName, hideEtc = True, deadLine = 1):
+    def Pirvate_GetCategoryDict(self, data, mainName, subName, deadLine = 1):
         mainCategory = set(data[mainName])
         result = {}
         for main in mainCategory:
-            result[main] = self.GetCategoryByCount(subName, filter=(data[mainName] == main), deadLine=deadLine)
+            result[main] = self.GetCategoryByCount(subName, filter=(data[mainName] == main), deadLine=2)
         return result
 
     def GetCategoryDict(self, mainName, subName):
@@ -36,13 +36,13 @@ class Region:
         self.HideEtc(dic, deadLine=deadLine)
         return {k: v for k, v in sorted(dic.items(), key=lambda item: item[1])}
 
-    def HideEtc(self, dic, deadLine = 1):
+    def HideEtc(self, dic, deadLine = 5):
         total = 0
         for i in dic:
             total += dic[i]
         for i in dic.copy():
-            percent = dic[i] / float(total) * 100.0
-            if(percent < deadLine):
+            percent = (dic[i] / float(total)) * 100.0
+            if(percent <= deadLine):
                 if('기타' in dic):
                    dic['기타'] += dic[i]
                 else:
@@ -52,16 +52,21 @@ class Region:
                 
 
 class GraphDrawer:
-    def ShowDetailGraphs(self, dict):
+    def ShowDetailGraphs(self, dict, type = 'pie', title = ''):
         length = len(dict)
         ySize = round(math.sqrt(length))
         xSize = math.ceil(math.sqrt(length))
-        fig, axs = plt.subplots(ySize, xSize, figsize=(15,15), squeeze=False)
-
+        
+        fig, axs = plt.subplots(ySize, xSize, figsize=(140, 140), squeeze=False, dpi=120)
+        matplotlib.rcParams.update({'font.size': 15})
+        #fig.tight_layout()
         currentX = 0
         currentY = 0
         for main in dict:
-            self.DrawBar(dict[main], axs[currentY, currentX], main)
+            if(type == 'bar'):
+                self.DrawBar(dict[main], axs[currentY, currentX], main+title)
+            elif(type == 'pie'):
+                self.DrawPie(dict[main], axs[currentY, currentX], main+title)
 
             if(currentX < xSize-1):
                 currentX += 1
@@ -76,7 +81,8 @@ class GraphDrawer:
         length = len(regions)
         ySize = round(math.sqrt(length))
         xSize = math.ceil(math.sqrt(length))
-        fig, axs = plt.subplots(ySize, xSize, figsize=(40,40), squeeze=False)
+        fig, axs = plt.subplots(ySize, xSize, figsize=(60,60), squeeze=False, dpi=80)
+        matplotlib.rcParams.update({'font.size': 13})
 
         for i in axs:
             for graph in i:
@@ -89,21 +95,20 @@ class GraphDrawer:
                 else:
                     break 
         self.Show()
-                
         
     def Show(self):
-        plt.subplots_adjust(bottom=0.15, wspace=0.7, hspace=0.7)
+        plt.subplots_adjust(bottom=0.2, left=0.05)
         plt.show()
 
     def DrawBar(self, dict, ax, title):
         pair = self.DictoinaryToPair(dict)
         ax.bar(pair[0], pair[1])
-        ax.set_title(title)
+        ax.set_title(title, fontsize=25)
         ax.tick_params(labelrotation=70)
 
     def DrawPie(self, dict, ax, title):
         pair = self.DictoinaryToPair(dict)
-        ax.set_title(title)
+        ax.set_title(title, fontsize=25)
         ax.pie(pair[1], labels=pair[0], autopct='%.1f%%')
 
     def DictoinaryToPair(self, dic):
@@ -122,8 +127,8 @@ for i in os.listdir():
     if(keyWord in i):
         regions.append(Region(i))
 
+#GraphDrawer().ShowRegionGraphs(regions, '상권업종대분류명', 'pie', deadLine=3)
+#GraphDrawer().ShowRegionGraphs(regions, '상권업종대분류명', 'bar', deadLine=3)
 
-# GraphDrawer().ShowRegionGraphs(regions, '상권업종대분류명', 'pie', deadLine=3)
-
-#GraphDrawer().ShowDetailGraphs(regions[0].GetCategoryDict('시군구명', '상권업종대분류명'))
-GraphDrawer().ShowDetailGraphs(regions[0].GetCategoryDict_HasFilt(['상권업종대분류명', '음식'], '상권업종대분류명', '상권업종소분류명'))
+#GraphDrawer().ShowDetailGraphs(regions[0].GetCategoryDict('시군구명', '상권업종대분류명'), 'bar')
+GraphDrawer().ShowDetailGraphs(regions[0].GetCategoryDict_HasFilt(['상권업종대분류명', '음식'], '상권업종대분류명', '상권업종소분류명'), type='bar', title = " : 제주도")
